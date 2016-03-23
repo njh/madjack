a56317f39e733e0367623581fd2048b123b9b12e
Nicholas Humfrey <njh@aelius.com>
2016-03-23 18:07:24 +0000

    Changed name to Multifunctional and License to MIT

--- perl/lib/Audio/MadJACK.pm
+++ perl/lib/Audio/MadJACK.pm
@@ -288,7 +288,7 @@ Audio::MadJACK - Talk to MadJACK server using Object Oriented Perl
 =head1 DESCRIPTION
 
 The Audio::MadJACK module uses Net::LibLO to talk to a 
-MadJACK (MPEG Audio Deck) server. It has an Object Oriented style 
+MadJACK (Multifunctional Audio Deck) server. It has an Object Oriented style 
 API making it simple to control multiple decks from a single script.
 
 
@@ -410,16 +410,24 @@ Nicholas J. Humfrey <njh@aelius.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005 Nicholas J. Humfrey
+Copyright (C) 2005-2016 Nicholas J. Humfrey
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
 =cut
