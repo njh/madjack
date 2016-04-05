477f63d0ebee864122757c8cab3659f799fefa4c
Nicholas Humfrey <njh@aelius.com>
2016-04-05 19:45:12 +0100

    Removed support for replies from madjack-remote

--- src/madjack-remote.c
+++ src/madjack-remote.c
@@ -14,9 +14,6 @@
 #include "config.h"
 
 
-#define REPLY_TIMEOUT	(1000)		// Number of milliseconds to wait for a reply
-
-
 // Display how to use this program
 static
 void usage( )
@@ -32,51 +29,7 @@ void usage( )
 	printf("  cue [<cuepoint>]  Cue deck (option cuepoint in seconds)\n");
 	printf("  eject             Eject the current track from deck\n");
 	printf("  load <filepath>   Load <filepath> into deck\n");
-	printf("  state             Get deck state\n");
-	printf("  position          Get playback position (in seconds)\n");
-	printf("  filepath          Get path of the currently loaded file\n");
-	printf("  ping              Check deck is still there\n");
 	exit(1);
-}
-
-
-static
-void error_handler(int num, const char *msg, const char *path)
-{
-    fprintf(stderr, "liblo error %d in path %s: %s\n", num, path, msg);
-}
-
-static
-int state_handler(const char *path, const char *types, lo_arg **argv, int argc,
-		 lo_message msg, void *user_data)
-{
-	printf("State: %s\n", &argv[0]->s);
-    return 0;
-}
-
-static
-int position_handler(const char *path, const char *types, lo_arg **argv, int argc,
-		 lo_message msg, void *user_data)
-{
-	printf("Position: %2.2f\n", argv[0]->f);
-    return 0;
-}
-
-static
-int filepath_handler(const char *path, const char *types, lo_arg **argv, int argc,
-		 lo_message msg, void *user_data)
-{
-	printf("Filepath: %s\n", &argv[0]->s);
-    return 0;
-}
-
-
-static
-int ping_handler(const char *path, const char *types, lo_arg **argv, int argc,
-		 lo_message msg, void *user_data)
-{
-	printf("Pong.\n");
-    return 0;
 }
 
 
@@ -86,8 +39,6 @@ int main(int argc, char *argv[])
 	char *port = NULL;
 	char *url = NULL;
 	lo_address addr = NULL;
-	lo_server serv = NULL;
-	int need_reply = 0;
 	int result = -1;
 	int opt;
 
@@ -125,79 +76,41 @@ int main(int argc, char *argv[])
 	else		addr = lo_address_new_from_url(url);
 
 
-	// Create a server for receiving replies on
-    serv = lo_server_new(NULL, error_handler);
-	lo_server_add_method( serv, "/deck/state", "s", state_handler, addr);
-	lo_server_add_method( serv, "/deck/position", "f", position_handler, addr);
-	lo_server_add_method( serv, "/deck/filepath", "s", filepath_handler, addr);
-	lo_server_add_method( serv, "/pong", "", ping_handler, addr);
-
-
 	// Check to see what message to send
 	if (strcmp( argv[0], "play") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/play", "");
+		result = lo_send(addr, "/deck/play", "");
 	} else if (strcmp( argv[0], "pause") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/pause", "");
+		result = lo_send(addr, "/deck/pause", "");
 	} else if (strcmp( argv[0], "stop") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/stop", "");
+		result = lo_send(addr, "/deck/stop", "");
 	} else if (strcmp( argv[0], "cue") == 0) {
 		// Check for argument
 		if (argc>=2) {
 			float cuepoint = atof( argv[1] );
-			result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/cue", "f", cuepoint);
+			result = lo_send(addr, "/deck/cue", "f", cuepoint);
 		} else {
-			result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/cue", "");
+			result = lo_send(addr, "/deck/cue", "");
 		}
 	} else if (strcmp( argv[0], "eject") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/eject", "");
+		result = lo_send(addr, "/deck/eject", "");
 	} else if (strcmp( argv[0], "load") == 0) {
 		// Check for argument
 		if (argc!=2) usage( );
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/load", "s", argv[1]);
-	} else if (strcmp( argv[0], "state") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/get_state", "");
-		need_reply=1;
-	} else if (strcmp( argv[0], "position") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/get_position", "");
-		need_reply=1;
-	} else if (strcmp( argv[0], "filepath") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/deck/get_filepath", "");
-		need_reply=1;
-	} else if (strcmp( argv[0], "ping") == 0) {
-		result = lo_send_from(addr, serv, LO_TT_IMMEDIATE, "/ping", "");
-		need_reply=1;
+		result = lo_send(addr, "/deck/load", "s", argv[1]);
 	} else {
 		fprintf(stderr, "Unknown command '%s'.\n", argv[0]);
 		usage();
 	}
+
+  lo_address_free( addr );
 	
 	if (result<1) {
 		fprintf(stderr, "Error: failed to send OSC message (error=%d).\n", result);
 		return -2;
-	} else if (need_reply) {
-		result = lo_server_recv_noblock(serv, REPLY_TIMEOUT);
-
-		// Did we get a reply ?
-		if (result==0) {
-			fprintf(stderr, "Error: didn't recieve a reply from MadJACK server after %d milliseconds.\n", REPLY_TIMEOUT);
-			return -1;
-		} else if (result<0) {
-			fprintf(stderr, "Error: failed to read reply (error=%d).\n", result);
-			return -2;
-		} else {
-			// Success
-			return 0;
-		}
 	} else {
 		// Success
 		return 0;
 	}
-	
-	
-    lo_address_free( addr );
-    lo_server_free( serv );
-
-    return 0;
 }
 
 
